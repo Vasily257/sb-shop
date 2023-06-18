b097107f08e4920fa97f593662284cf6d0c1a627
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-06-18 19:01:55 +0400

    feat(basket section): add the total-cost include

--- src/templates/lib/functions.js
+++ src/templates/lib/functions.js
@@ -2,4 +2,21 @@ module.exports = {
 	addAwesome: function (str) {
 		return str + ' Awesome!';
 	},
+
+	/** Добавить разделитель для чисел */
+	formatNumber: function (number) {
+		number = number.toString();
+		let result = '';
+
+		for (let k = number.length - 1, c = 0; k >= 0; k--, c++) {
+			if (c === 3) {
+				c = 0;
+				result = number.substr(k, 1) + ' ' + result;
+			} else {
+				result = number.substr(k, 1) + result;
+			}
+		}
+
+		return result;
+	},
 };
