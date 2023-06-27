b59a040ad0ac0390676bce7fd64c9d411a5706ec
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-06-27 19:43:25 +0400

    feat: init the TotalCost

--- src/assets/js/components/init.js
+++ src/assets/js/components/init.js
@@ -1,7 +1,10 @@
 import Basket from '../classes/Basket';
 import Products from '../classes/Products';
+import TotalCost from '../classes/TotalCost';
 
 const basket = new Basket();
 const products = new Products(basket);
+const totalCost = new TotalCost(basket);
 
 products.init();
+totalCost.init();
